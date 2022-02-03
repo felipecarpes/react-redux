55d53953cb4a5459b15b359e505842cbef0fc6d4
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-03 01:05:44 -0300

    creating saga to verify stock of products

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,9 +1,33 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, select, takeLatest, call, put } from 'redux-saga/effects'
+import { addProductToCartFailure, addProductToCartRequest, addProductToCartSuccess } from './action'
+import { IState } from '../..';
+import api from '../../../services/api';
+import { AxiosResponse } from 'axios';
+import { ActionTypes } from './types';
 
-function checkProductStock() {
-    console.log('Adicionou ao carrinho') 
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>
+
+interface IStockResponse {
+    id: number;
+    quantity: number;
+}
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+    const { product } = payload;
+
+    const currentQuantity: number = yield select((state: IState) => {
+        return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+    })
+
+    const availableStockResponse: AxiosResponse<IStockResponse> = yield call(api.get, `stock/${product.id}`);
+
+    if(availableStockResponse.data.quantity > currentQuantity) {
+        yield put(addProductToCartSuccess(product))
+    } else {
+        yield put(addProductToCartFailure(product.id))
+    }
 }
 
 export default all([
-    takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+    takeLatest(ActionTypes.addProductToCartRequest, checkProductStock)
 ])
